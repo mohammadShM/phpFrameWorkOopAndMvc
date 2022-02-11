4d61d699d89be0fcb6f7fec74d38877206cd4258
mohammadShM <mohammadshm1365@gmail.com>
2022-02-12 00:14:21 +0330

    create gitignore

---
+++ storage/views/93ecf3b174f939bfa6a77380d8a7bec6f7b77691.php
@@ -0,0 +1,8 @@
+
+<?php $__env->startSection("title"); ?>
+ Server Error
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection("content"); ?>
+ <h1>Server Error</h1>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\MohammadShM\Desktop\ProjectMe\phpMvc2\App\Views/errors/500.blade.php ENDPATH**/ ?>
